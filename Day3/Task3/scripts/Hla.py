f27dcbf4dd288b4f46d5dcfb32eec560e1192880
Hla Mansour <40806663+hlaahmed@users.noreply.github.com>
2020-08-14 03:18:31 +0200

    copy file .cpp ,py and run them ith ros commands

---
+++ Day3/Task3/scripts/Hla.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+print("i'm software robotics engineer, and now learning ROS")
+ 
---
+++ Day3/Task3/src/Hla.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+int main()
+{
+    std::cout<< "i'm software robotics engineer, and now learning ROS"<<std::endl;
+    return 0;
+}
