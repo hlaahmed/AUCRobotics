296a576a9ab1d0e32bdffc6b7dd77e1cbea9a5b7
Hla Mansour <40806663+hlaahmed@users.noreply.github.com>
2020-08-17 04:03:25 +0200

    Gaussian equation code and result screenshot

---
+++ Day3/Hla.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+double f(double mu, double sigma2, double x)
+{
+    double prob = (1/sqrt(2*M_PI*sigma2))*exp(-0.5*pow(x-mu,2)*sigma2);
+    return prob;
+}
+int main()
+{
+    cout<<f(10.0,4.0,8.0)<<endl;
+    return 0;
+}
