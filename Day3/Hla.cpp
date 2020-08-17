c2e3f6d78fd703073474f57de715fa2b8ad6eaa6
Hla Mansour <40806663+hlaahmed@users.noreply.github.com>
2020-08-17 04:10:08 +0200

    Update Hla.cpp

--- Day3/Hla.cpp
+++ Day3/Hla.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 double f(double mu, double sigma2, double x)
 {
-    double prob = (1/sqrt(2*M_PI*sigma2))*exp(-0.5*pow(x-mu,2)*sigma2);
+    double prob = (1/sqrt(2*M_PI*sigma2))*exp(-0.5*pow(x-mu,2)/sigma2);
     return prob;
 }
 int main()
