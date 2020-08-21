5d2fad69f3e1ed436ac36db55fe3a3a045f4abcf
Hla Mansour <40806663+hlaahmed@users.noreply.github.com>
2020-08-21 22:13:49 +0200

    server

---
+++ Day4/scripts/service_server.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import rospy
+
+from Day4.srv import WordCount,WordCountResponse
+print("waiting ........")
+
+def count_words(request):
+    return WordCountResponse(len(request.words.split()))
+
+
+rospy.init_node('service_server')
+
+service = rospy.Service('word_count', WordCount, count_words)
+
+rospy.spin()
